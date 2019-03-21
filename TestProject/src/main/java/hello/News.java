b72696c9e50c647898d1cd25b8fc565b05b60635
Volf-Vladislav <therealmiyutreaz@gmail.com>
2019-03-21 21:13:53 +0300

    maven

---
+++ TestProject/src/main/java/hello/News.java
@@ -0,0 +1,46 @@
+package hello;
+
+import java.util.Objects;
+
+public class News {
+    private String title;
+    private String text = "";
+
+    public News(String title) {
+        this.title = title;
+    }
+
+    public News(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    @Title
+    public String getTitle() {
+        return title;
+    }
+
+    @Content
+    public String getText() {
+        return text;
+    }
+
+    public void addLine(String text) {
+        if(text != null)
+            this.text += text + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        News news = (News) o;
+        return Objects.equals(title, news.title) &&
+                Objects.equals(text, news.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, text);
+    }
+}
